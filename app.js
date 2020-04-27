02e85f5d51499708c1fd9f0a8ec6fb6ac2f0febb
wangjianpeng <707804052@qq.com>
2020-04-27 18:02:16 +0800

    feat: 配置logger

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const views = require('koa-views');
 const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser');
-const logger = require('koa-logger');
+const logger = require('./lib/logger').logger('app');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -19,28 +19,30 @@ app.use(bodyparser({
   enableTypes: [ 'json', 'form', 'text' ],
 }));
 app.use(json());
-app.use(logger());
 app.use(require('koa-static')(__dirname + '/public'));
 
 app.use(views(__dirname + '/views', {
   extension: 'pug',
 }));
 
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+try {
+  app.use(async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    logger.info(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  });
+} catch (error) {
+  logger.error('start server failed =>', error);
+}
 
 // routes
 app.use(index.routes(), index.allowedMethods());
 app.use(users.routes(), users.allowedMethods());
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx);
+app.on('error', error => {
+  logger.error('app on error =>', error);
 });
 
 module.exports = app;
