cd981578e1b4d17e186247856e40811d24f8cbda
wangjianpeng <707804052@qq.com>
2020-04-28 13:40:35 +0800

    feat: 配置apidoc

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * @api {POST} /api/user 创建用户
+ * @apiGroup user
+ * @apiParam  {String} username 账号
+ * @apiParam  {String} password 密码
+ * @apiParam  {String} name 名字
+ */
+
 exports.create = async ctx => {
   const data = ctx.request.body;
   const rules = {
